a1b590b8c048959ba5275374dbad7399c7ec8685
Katerina <e.mayakovskaya@gmail.com>
2025-10-21 21:45:59 +0300

    Register models in admin panel

--- foodplan_site/admin.py
+++ foodplan_site/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from foodplan_site.models import RecipeIngredient, Recipe, Ingredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+    fields = ('ingredient', 'quantity', 'unit')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
