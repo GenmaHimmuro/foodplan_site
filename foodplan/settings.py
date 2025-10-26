b3c61aa7933838b8fdef876bb19908f3cba110ba
Katerina <e.mayakovskaya@gmail.com>
2025-10-26 21:28:55 +0200

    Add media urls for debug mode

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -88,6 +88,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  
