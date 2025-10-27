be6eafe490db91fb00f9cf8ff383b16f93f3ab25
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-10-27 03:14:13 -0400

    Initial commit from server

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['prekrasno-zamechatelno.ru', 'www.prekrasno-zamechatelno.ru','77.110.109.187', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
